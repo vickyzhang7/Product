1cfe30efdf63202bce463920411689a05465c3e2
Weiqi Zhang <weiqi997998@gmail.com>
2024-06-25 01:02:13 -0400

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,15 +7,12 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div className="navbar">
-    <nav>
-    <Link>
-
-        
-    </Link>
-
-
-    </nav>
-      
+      <nav>
+        <Link to="/" className="brand">
+          <h1>Cooking Ninja</h1>
+        </Link>
+        <Link to="/create">Create Recipe</Link>
+      </nav>
     </div>
   )
 }
