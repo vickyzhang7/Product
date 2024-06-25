df2d2d065c32fe07a9a73ab8069e4e989d714dc0
Weiqi Zhang <weiqi997998@gmail.com>
2024-06-25 16:37:56 -0400

    RecipeList

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { useFetch } from '../../hooks/useFetch'
+import RecipeList from '../../components/RecipeList'
 import './Home.css'
-
-
 
 export default function Home() {
   const {data, isPending, error}= useFetch('http://localhost:5001/recipes')
@@ -10,10 +9,10 @@ export default function Home() {
     <div className="home">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {data && data.map(recipe => (
-        <h2 key={recipe.id}>{recipe.title}</h2>
-      ))}
+      {data && <RecipeList recipes={data} />}
 
     </div>
   )
 }
+
+
