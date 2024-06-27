3bded551470de36b7279cac76a7427a9b83eab86
Weiqi Zhang <weiqi997998@gmail.com>
2024-06-27 16:51:42 -0400

    SearchBar

---
+++ src/components/Searchbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import './Searchbar.css'
+
+export default function Searchbar() {
+    const [search, setSearch] = useState('')
+    const navigate = useNavigate()
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // navigate(`/search/${search}`)
+        navigate(`/search?q=${search}`)
+    }
+
+  return (
+    <div className="searchbar">
+      <form onSubmit={handleSubmit}>
+        <input 
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  )
+}
+
+
